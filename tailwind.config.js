0fbd9134ec0b097de4a952ead796cfa32042f16c
FrancoCunha <francunhaduarte@gmail.com>
2024-10-11 00:48:16 -0300

    New design and start aboutme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      black: '#141414',
-      lightblack: '#1C1C1C',
-      white: '#FFFFFF',
-      gray: '#898989',
-      darkgray: '#525252',
-      green: '#12793a',
-      blue: '#213d91'
-    },
     screens: {
       sm: '480px',
       md: '768px',
@@ -22,9 +13,20 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        'transparent': 'linear-gradient(to right, rgba(173, 173, 173, 0.20), rgba(255, 255, 255, 0.20))',
         'bg-default-projects': 'linear-gradient(0deg, rgba(20,20,20,1) 0%, rgba(20,20,20,1) 50%, rgba(36,36,36,1) 100%)',
         'projects': 'linear-gradient( 305deg, hsl(0deg 0% 48%) 0%, hsl(0deg 0% 40%) 20%, hsl(0deg 0% 30%) 40%, hsl(0deg 0% 30%) 60%, hsl(0deg 0% 40%) 80%, hsl(0deg 0% 48%) 100% )'
+      },
+      colors: {
+        black: {
+          100: '#141414',
+          200: '#161616',
+          300: '#1C1C1C'
+        },
+        white: '#FFFFFF',
+        gray: '#898989',
+        darkgray: '#525252',
+        green: '#12793a',
+        blue: '#213d91'
       }
     },
   },
