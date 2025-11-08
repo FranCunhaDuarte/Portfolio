51fabd087aa79cdf832eeea3aa518309e33fb198
FrancoCunha <francunhaduarte@gmail.com>
2025-11-08 15:48:08 -0300

    Fixing proyects links ref

--- src/components/LinkShineExternal.jsx
+++ src/components/LinkShineExternal.jsx
@@ -21,7 +21,7 @@ const LinkShineExternal = ({link = null,label = 'Undefined'}) => {
 
   return (
     <>
-        <div onMouseEnter={shineAnimation} className='w-[120px] xl:w-[130px] scale-[.85] xl:scale-100'><div className='bg-[#1F1F1F] mx-auto w-min px-4 py-1.5 rounded-[10px] border-[#a7a7a7] border text-white overflow-hidden flex items-center group relative' href={link} rel="noopener noreferrer" target='blank'><div ref={shine} className='absolute h-[120%] w-[15px] bg-white opacity-10 top-[50%] translate-y-[-50%] -left-5 rotate-12'></div><span className='xl:pr-0 xl:group-hover:pr-2 pr-2 duration-300 text-nowrap'>{label}</span><ArrowCorner className='w-[10px] xl:max-w-0 overflow-hidden xl:opacity-0 transition-all duration-300 xl:group-hover:max-w-[10px] opacity-100 xl:group-hover:opacity-100' /></div></div>
+        <div onMouseEnter={shineAnimation} className='w-[120px] xl:w-[130px] scale-[.85] xl:scale-100'><a className='bg-[#1F1F1F] mx-auto w-min px-4 py-1.5 rounded-[10px] border-[#a7a7a7] border text-white overflow-hidden flex items-center group relative' href={link} rel="noopener noreferrer" target='blank'><div ref={shine} className='absolute h-[120%] w-[15px] bg-white opacity-10 top-[50%] translate-y-[-50%] -left-5 rotate-12'></div><span className='xl:pr-0 xl:group-hover:pr-2 pr-2 duration-300 text-nowrap'>{label}</span><ArrowCorner className='w-[10px] xl:max-w-0 overflow-hidden xl:opacity-0 transition-all duration-300 xl:group-hover:max-w-[10px] opacity-100 xl:group-hover:opacity-100' /></a></div>
     </>
   )
 }
